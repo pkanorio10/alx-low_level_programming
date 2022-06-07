bfca551bf2bd74b0dff192ead86e686c34f61d49
Purity Kanorio <kanorio2010@gmail.coom>
2022-06-07 12:25:43 -0700

    task0

--- 0x02-functions_nested_loops/0-purchar.c
+++ 0x02-functions_nested_loops/0-purchar.c
@@ -7,6 +7,7 @@
  */
 int main (void)
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -15,6 +16,6 @@ int main (void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n);
-	return 0;
+	_putchar('\n');
+	return (0);
 }
