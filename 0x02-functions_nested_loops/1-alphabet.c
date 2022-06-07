1790713848eccee852b0b9e6f94834e1b66e5a94
Purity Kanorio <kanorio2010@gmail.coom>
2022-06-07 14:56:13 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_alphabet - Function that prints alphabets
+ * Descripion: to print alphabets
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int count;
+	for (count = 'a'; count <= 'z'; count++)
+	{
+		_putchar(count);
+	}
+	_putchar('\n');
+}
