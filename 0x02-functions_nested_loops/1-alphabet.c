95602d97e5be5d77bed0bab84603b581a99f7ae2
Purity Kanorio <kanorio2010@gmail.coom>
2022-06-07 14:58:26 -0700

    task2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,10 +8,10 @@
  */
 void print_alphabet(void)
 {
-	int count;
-	for (count = 'a'; count <= 'z'; count++)
-	{
-		_putchar(count);
-	}
-	_putchar('\n');
+int count;
+for (count = 'a'; count <= 'z'; count++)
+{
+_putchar(count);
+}
+_putchar('\n');
 }
