fe8612410135fe522fe38d8f63f8ed1c702493b6
Purity Kanorio <kanorio2010@gmail.coom>
2022-06-08 10:25:02 -0700

    tack5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,28 @@
 #include "main.h"
+
 /**
- *  print_sign - prints the sign of a number
- *  @n: number being tested
- * Return: Always 0.
+ * print_sign - checks for integer sign
+ * @n: integer to check
+ * Return: 0 or 1
  */
 
 int print_sign(int n)
 {
+
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('-');
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
 		return (-1);
 
 	}
-	return (0);
 }
