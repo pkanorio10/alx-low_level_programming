e6fe386eef1caf9b19cc5131afc63ebd19edc9c0
Purity Kanorio <kanorio2010@gmail.coom>
2022-06-08 08:14:54 -0700

    (void print_to_98(int n);

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints from any integer to 98
+ * @n: integer input
+ *
+ * @n: integer input
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%i, ", n);
+		if (n > 98)
+			if (n > 98)
+		else
+			n++;
+	}
+	printf("98\n");
+
+}
