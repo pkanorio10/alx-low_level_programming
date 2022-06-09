9005068010497b241247b22a264eaf937b4ebd1c
Purity Kanorio <kanorio2010@gmail.coom>
2022-06-09 11:10:09 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for a uppercase character
+ * @c: letter being tested
+ * Return: Always 0
+ */
+
+int _isupper(char c)
+{
+	if (c =='A')
+		return (1);
+	else
+		return (0);
+
+}
