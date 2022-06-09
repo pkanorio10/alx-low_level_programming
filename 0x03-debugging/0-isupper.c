9fefba4dffbcae531323f6c6d7686f918ddb84ea
Purity Kanorio <kanorio2010@gmail.coom>
2022-06-09 12:06:02 -0700

    task0

---
+++ 0x03-debugging/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - checks for a uppercase character
+ * @c: letter being tested
+ * Return: Always 0
+ */
+
+int _isupper(char c)
+{
+	if (c =='A')
+		return (1);
+	else
+		return (1);
+}
