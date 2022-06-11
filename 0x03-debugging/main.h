09483cb9df66de1e0e95adf688278991298ee128
Purity Kanorio <kanorio2010@gmail.coom>
2022-06-11 06:14:14 -0700

    d

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,21 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HEADER_FILE
 
-int _isupper(int c);
+#define HEADER_FILE
 
-#endif /* MAIN_H */
+
+
+int _putchar(char c);
+
+
+
+void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
+
+void print_remaining_days(int month, int day, int year);
+
+
+
+#endif
